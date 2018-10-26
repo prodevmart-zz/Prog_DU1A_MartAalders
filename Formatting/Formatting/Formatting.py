b2f89360bb57cd67d4beab94ee8f43c055f6d41d
Martino Aalders <mart.aalders@student.hu.nl>
2018-10-26 19:29:34 +0200

    Formatting version 2

--- Formatting/Formatting/Formatting.py
+++ Formatting/Formatting/Formatting.py
@@ -1,4 +1,12 @@
-def convert(Celcius):
-	Fahrenheit = Celcius * 1.8 + 32  
-	return str(Fahrenheit) + " °F"
-print(convert(25))
+def convert(temp):
+    F = temp * 1.8 + 32
+    return F
+
+def table():
+    print('   {:7}{}'.format('F', 'C'))
+    for i in range(-30, 41, 10):
+        C = i
+        Far = convert(i)
+        print('{:5}   {:5}'.format(Far, float(C)))
+
+table()
