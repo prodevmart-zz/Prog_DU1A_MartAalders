dc286ae020fa25d291c4de579f8363caf609f609
Martino Aalders <mart.aalders@student.hu.nl>
2018-10-31 14:23:14 +0100

    Ns assignment

--- Final Assignment NS functies/Final Assignment NS functies/Final_Assignment_NS_functies.py
+++ Final Assignment NS functies/Final Assignment NS functies/Final_Assignment_NS_functies.py
@@ -1,38 +1,44 @@
-def standaardprijs(afstandKM):
-
-# 0.80 cent per treinrit
-
-	if afstandKM > 50:
-		prijs = 15 + 0.60 * afstandKM
-		return prijs
-	elif afstandKM < 0:
-		prijs = 0
-		return prijs
-
-	else:
-		prijs = 0.80 * afstandKM
-		return prijs
+afstandKM = int(input('Hoeveel Kilometer\'s ga je reizen?')) # input voor de hoeveelheid kilometer
+weekendrit_invoer = input("is het nu weekend?") #input voor of het weekend is
+leeftijd = int(input("Hoe oud ben je?")) #input voor de leeftijd
 
 
-def ritprijs(leeftijd, weekendrit, afstandKM):
-	
-	prijs = standaardprijs(afstandKM)
-	if leeftijd < 12 or leeftijd > 65:
-	#30 Korting
-	#35 procent korting vanwege weekend
-		prijs / 100 * 65
-		return prijs
-	elif leeftijd < 12 or leeftijd > 65 and weekendrit == False:
-		return (prijs / 100) * 70
-	elif leeftijd < 12 or leeftijd > 65 and weekendrit == True:
-		return (prijs / 100) * 65
-	elif weekendrit == True:
-		prijs = ((prijs  / 100 * 40) / 100) * 60
-		return prijs
-	else:
-		prijs = prijs  / 100 * 40
-		return prijs
+if weekendrit_invoer == 'ja': # als de input ja is dan is het weekend
+    weekend = True #maak de boolean weekend True
+else: # maak in elk ander geval 
+    weekend = False # maak de boolean weekend False
 
+def standaardprijs(afstandKM): # De  functie standaardprijs\
+    if afstandKM <= 0:
+        tarief = 0
+    elif afstandKM < 50:
+        tarief = afstandKM * 0.80
+    else:
+        tarief = 15 + afstandKM * 0.60
 
-print(ritprijs(66, True, 120))
+    return tarief
 
+def ritprijs(afstandKM, weekend, leeftijd):
+    tarief = standaardprijs(afstandKM)
+    if (leeftijd <12) or (leeftijd > 64):
+        if weekend == True:
+            tarief = tarief * 0.65
+            return round(tarief,2)
+        else:
+            tarief  = tarief * 0.70
+        return round(tarief, 2)
+    else:
+        if weekend == True:
+            tarief = tarief * 0.60
+            return round(tarief, 2)
+        else:
+            return round(tarief, 2)
+
+print("Deze rit gaat €" + str(ritprijs(afstandKM, weekend, leeftijd)) + " kosten.")
+
+def bsprijs(afstandKM, weekend, leeftijd):
+    tarief = standaardprijs(afstandKM)
+    if (leeftijd == 69):
+        print("kek je leeftijd")
+    else:
+        print("je moet die kek leeftijd hebben man ")
