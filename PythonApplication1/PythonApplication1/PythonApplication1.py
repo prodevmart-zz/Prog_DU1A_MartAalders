bf2438efcf6e1ec0c633e5e5d15877f089cfcf16
Martino Aalders <mart.aalders@student.hu.nl>
2018-10-31 14:20:25 +0100

    Trest

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,5 @@
+'prints values in 2D list t as a 2D table'
+for row in t:
+	for item in row: # print item followed by
+		print(item, end=' ') # a blank space
+print() # move to next line
---
+++ Two dimensional lists/Two dimensional lists/Two_dimensional_lists.py
@@ -0,0 +1,18 @@
+studentencijfers = [ [95, 92, 86],[66, 75, 54],[89, 72, 100],[34, 0, 0] ] 
+
+def gemiddelde_per_student(studentencijfers): 
+   'prints n lines each containing value 0 1 2 ... n-1'
+	for j in range(n): # repeat n times:
+		for i in range(n): # print 0, 1, ..., n-1
+			print(i, end = ' ')
+			return antw 
+	print()
+   
+
+def gemiddelde_van_alle_studenten(studentencijfers): 
+   ...
+   return antw 
+
+print(gemiddelde_per_student(studentencijfers)) 
+
+print(gemiddelde_van_alle_studenten(studentencijfers))
